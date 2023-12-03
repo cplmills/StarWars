345c04f48aaa2cbcf1833113903c76a2ae22def5
cplmills <91394219+cplmills@users.noreply.github.com>
2023-12-03 20:48:02 +1000

    lint

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -6,27 +6,19 @@ import { getAssetFromExternal } from "./external_services.mjs";
 export async function favoritesHandler(e) {
     // adds a favorite if add = true, otherwise removes it
     let originalText = e.target.innerHTML;
-    console.log(originalText);
     e.target.innerHTML = "Processing...";
     if (e.target.dataset.action === "add") {
         const item = await getAssetFromExternal(e.target.dataset.id, getParam("category"), "data");
         await addItemToFavorites(item);
-        e.target.setAttribute("class", "tile-button hidden");
         let showRemoveButton = document.querySelector(`[data-id="${e.target.dataset.id}"][data-action="remove"]`);
         showRemoveButton.setAttribute("class", "tile-button");
     } else {
         await removeItemFromFavorites(e.target.dataset.id);
-        e.target.setAttribute("class", "tile-button hidden");
         let showButton = document.querySelector(`[data-id="${e.target.dataset.id}"][data-action="add"]`);
         showButton.setAttribute("class", "tile-button");
     }
-
-    // let hideButton = document.querySelector(`[data-id="${e.target.dataset.id}"][data-action="add"]`);
-    // hideButton.setAttribute("class", "tile-button hidden");
+    e.target.setAttribute("class", "tile-button hidden");
     e.target.innerHTML = originalText;
-
-    // let showButton = document.querySelector(`[data-id="${e.target.dataset.id}"][data-action="remove"]`);
-    // showButton.setAttribute("class", "tile-button");
     loadNavBar();
 }
 
@@ -47,14 +39,11 @@ export function addItemToFavorites(item) {
 
     // Check if the item is already in the cart
     const existingItemIndex = favoriteItems.findIndex(
-      (element) => {
-          console.log(item);
-        element.results[0].name === item.results[0].name;
-    });
+      (element) => element.results[0].name === item.results[0].name);
+
     if (existingItemIndex === -1) {
       // If the item is not in the cart, add it with a quantity of 1
       favoriteItems.push(item);
-      console.log("added item to favorites");
       // Update the cart in local storage
       setLocalStorage("sw-favorites", favoriteItems);
     }
@@ -64,7 +53,6 @@ export function checkForItemInFavorites(item) {
     const favoriteItems = getLocalStorage("sw-favorites") || [];
     const existingItemIndex = favoriteItems.findIndex(
       (element) => element.results[0].name === item);
-    console.log(existingItemIndex);
 
     if (existingItemIndex === -1) return false;
     return true;  
