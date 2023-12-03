3f86d87800f6d8065a44455e0114d5185e1b293e
cplmills <91394219+cplmills@users.noreply.github.com>
2023-12-03 15:30:40 +1000

    remove from localstorage working

--- src/js/categories.mjs
+++ src/js/categories.mjs
@@ -1,5 +1,5 @@
 import { getAssetFromExternal, getAttributes, getCombinedJSON } from "./external_services.mjs";
-import { proper, addItemToFavorites, getParam, checkForItemInFavorites } from "./utils.mjs";
+import { proper, addItemToFavorites, getParam, checkForItemInFavorites, getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export async function addTile(data, selector, category) {
     // create a new tile to be added to the page
@@ -24,11 +24,12 @@ export async function addTile(data, selector, category) {
 
         const isInFavorites = checkForItemInFavorites(data);
         if (isInFavorites) {
+            // console.log(data.name + " is in favorites");
             newButton.innerText = "Remove From Favorites";
-            newButton.addEventListener("click", (e) => {addToFavoritesHandler(e)});
+            newButton.addEventListener("click", (e) => {favoritesHandler(e, false)});
         } else {
-           newButton.innerText = "☆ Add to Favorites";
-           newButton.addEventListener("click", (e) => {addToFavoritesHandler(e)});
+            newButton.innerText = "☆ Add to Favorites";
+           newButton.addEventListener("click", (e) => {favoritesHandler(e, true)});
         }
         newButton.setAttribute("data-id", data.name);
 
@@ -68,9 +69,25 @@ export function translateCategory(input) {
     }
 }
 
-async function addToFavoritesHandler(e) {
-    e.target.innerHTML = "Adding To Favorites";
-    const item = await getAssetFromExternal(e.target.dataset.id, getParam("category"), "data");
-    addItemToFavorites(item);
-    e.target.innerHTML = "Remove From Favorites";
+async function favoritesHandler(e, add) {
+    // adds a favorite if add = true, otherwise removes it
+    e.target.innerHTML = "Processing...";
+    console.log(add);
+    if (add) {
+        const item = await getAssetFromExternal(e.target.dataset.id, getParam("category"), "data");
+        addItemToFavorites(item);
+        e.target.innerHTML = "Remove From Favorites";
+    } else {
+        removeItemFromFavorites(e.target.dataset.id);
+        e.target.innerHTML = "☆ Add To Favorites";
+    }
+}
+
+
+function removeItemFromFavorites(id) {
+    const localArray = getLocalStorage("sw-favorites");
+    console.log(localArray);
+    const existingItemIndex = localArray.findIndex((element) => element.results[0].name === id);
+    localArray.splice(existingItemIndex, 1);
+    setLocalStorage("sw-favorites",localArray);
 }
